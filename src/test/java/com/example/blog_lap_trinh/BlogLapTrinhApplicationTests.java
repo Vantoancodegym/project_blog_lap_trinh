31d5a2da3435ed0072ddb9a1266b508f0feedd80
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-09 10:52:36 +0700

    khởi tạo dự án blog lập trình

---
+++ src/test/java/com/example/blog_lap_trinh/BlogLapTrinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_lap_trinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogLapTrinhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
